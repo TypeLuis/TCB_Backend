644d62df8c0c2dcbb69b25775d422bacfb6f2456
typeluis <typeluis@gmail.com>
2023-04-16 23:35:59 -0400

    logging

--- app.py
+++ app.py
@@ -67,7 +67,9 @@ def opscan_chapters():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--enable-javascript")
-        options.add_argument("--headless")
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+        # options.add_argument("--headless")
         driver = webdriver.Chrome(executable_path=os.environ.get(
             "CHROMEDRIVER_PATH"), options=options)
 
