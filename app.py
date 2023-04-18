66ab921a169bcc0601125d2cdde8ed7f13a418ba
typeluis <typeluis@gmail.com>
2023-04-18 00:01:55 -0400

    fixed OPSCANS

--- app.py
+++ app.py
@@ -33,7 +33,10 @@ cache = dict()
 
 
 def get_data(url):
-    page = requests.get(url).text
+    headers = {
+        'User-Agent': "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+    }
+    page = requests.get(url, headers=headers).text
     doc = bs(page, "html.parser")
     return doc
 
